8a998db118b464d22b3c2c43de33e002b5c60ed6
Char0x61 <Sw33tH34rt@protonmail.ch>
2023-05-03 19:43:45 +0200

    Hidding layers, renaming layers

--- client/src/EditorPage/Layers/ImageLayer.jsx
+++ client/src/EditorPage/Layers/ImageLayer.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useContext } from "react";
 import { EditorContext } from "../Editor";
 import "./ImageLayer.css";
@@ -6,6 +7,8 @@ function ImageLayer(props)
 {
 	const editor = useContext(EditorContext);
 	const previewImage = props.layer.canvas.toDataURL();
+	const [hidden, setHidden] = useState(props.layer.hidden);
+	const [title, settitle] = useState(props.layer.title);
 	const aspectW = props.layer.canvas.width > props.layer.canvas.height ? 1.0 : props.layer.canvas.width / props.layer.canvas.height;
 	const aspectH = props.layer.canvas.height > props.layer.canvas.width ? 1.0 : props.layer.canvas.height / props.layer.canvas.width;
 	return (
@@ -22,9 +25,15 @@ function ImageLayer(props)
 					height: `${aspectH*100}%`
 				}} alt={""}></img>
 			</div>
-			<input className="ImageLayerTitleInput" type="text" value={props.layer.title} onChange={()=>{/*silence error*/}}></input>
+			<input className="ImageLayerTitleInput" type="text" value={title} onChange={(e)=>{
+				settitle(e.target.value);
+				props.layer.title = e.target.value;
+			}}></input>
 			<div className="ImageLayerOptions">
-				<div className="ImageLayerOptionButton">H</div>
+				<div className="ImageLayerOptionButton" onClick={()=>{
+					setHidden(!props.layer.hidden);
+					props.layer.setHidden(!props.layer.hidden);
+				}}>{hidden ? "H" : "h"}</div>
 				<div className="ImageLayerOptionButton">D</div>
 				<div className="ImageLayerOptionButton">M</div>
 			</div>
--- client/src/ImageLayerInternal/ImageLayerInternal.js
+++ client/src/ImageLayerInternal/ImageLayerInternal.js
@@ -1,11 +1,23 @@
 
-export default function ImageLayerInternal(imageFile)
+export default class ImageLayerInternal
 {
-	this.canvas = document.createElement("canvas");
-	this.context = this.canvas.getContext("2d");
-	this.canvas.width = imageFile.width;
-	this.canvas.height = imageFile.height;
-	this.canvas.className = "CanvasLayer";
-	this.title = "Layer";
-	this.hidden = false;
+	constructor(imageFile) 
+	{
+		this.canvas = document.createElement("canvas");
+		this.context = this.canvas.getContext("2d");
+		this.canvas.width = imageFile.width;
+		this.canvas.height = imageFile.height;
+		this.canvas.className = "CanvasLayer";
+		this.title = "Layer";
+		this.hidden = false;
+	}
+
+	setHidden(hidden) 
+	{
+		if(this.hidden !== hidden) 
+		{
+			this.canvas.style.display = hidden ? "none" : "";
+			this.hidden = hidden;
+		}
+	}
 }
